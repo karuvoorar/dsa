df949e1dfd18dfc2a2c47a63d56fd88d2c232782
karuvoorar <rbkameshwar99@gmail.com>
2025-12-07 21:57:14 +0530

    code: linked list singly

--- src/linkedlist/fundamentals/singly/ListNode.java
+++ src/linkedlist/fundamentals/singly/ListNode.java
@@ -5,6 +5,12 @@ public class ListNode {
     int data;
     ListNode next;
 
+    public ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+
     public ListNode(int data, ListNode node) {
         this.data = data;
         this.next = node;
--- src/linkedlist/fundamentals/singly/SinglyLinkedList.java
+++ src/linkedlist/fundamentals/singly/SinglyLinkedList.java
@@ -6,8 +6,23 @@ public class SinglyLinkedList {
     static ListNode tail = null;
     static int size;
 
+    public static void insertHead(ListNode node) {
+        // if it's first then add as head
+        if (head == null) {
+            head = node;
+            tail = node;
+            ++size;
+            return;
+        }
+
+        // insert at first
+        node.next = head;
+        head = node;
+        ++size;
+    }
+
     // Insert at end
-    public static void insert(ListNode node) {
+    public static void insertTail(ListNode node) {
 
         // if it's first then add as head
         if (head == null) {
@@ -23,6 +38,30 @@ public class SinglyLinkedList {
         ++size;
     }
 
+    // Insert at specific position
+    public static void insertAt(ListNode node, int index) {
+
+        if (size == 0 || index == 0) {
+            insertHead(node);
+            return;
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        for (int i = 1; i < index && curr != null; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        prev.next = node;
+        node.next = curr;
+        ++size;
+
+        // if the index is big then add at last
+        if (curr == null) tail = node;
+    }
+
     // Delete a node
     public static void remove(int data) {
 
@@ -34,7 +73,6 @@ public class SinglyLinkedList {
         ListNode curr = head;
 
         while (curr != null) {
-
             if (curr.data == data) {
                 if (curr == head) {
                     head = head.next;
@@ -65,21 +103,22 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
 
+        ListNode node0 = new ListNode(0, null);
         ListNode node1 = new ListNode(1, null);
         ListNode node2 = new ListNode(2, null);
         ListNode node3 = new ListNode(3, null);
         ListNode node4 = new ListNode(4, null);
         ListNode node5 = new ListNode(5, null);
 
-        insert(node1);
+        insertTail(node1);
 
         // print 1 node
         printList();
 
-        insert(node2);
-        insert(node3);
-        insert(node4);
-        insert(node5);
+        insertTail(node2);
+        insertTail(node3);
+        insertTail(node4);
+        insertTail(node5);
 
         // print 5 nodes
         printList();
@@ -87,14 +126,11 @@ public class SinglyLinkedList {
         remove(3);
         printList();
 
-        remove(1);
+        insertHead(node0);
         printList();
 
-        remove(5);
+        insertAt(node3, 5);
         printList();
-
-        printList();
-
     }
 
 }
